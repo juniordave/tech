4145ab86619dd52f982a667105c6323bf5863ae5
juniordave <juniordave13@gmail.com>
2023-11-29 16:04:30 +0530

    tchnocit final commit

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
-var activeButton;
+// Initialize the default state
+toggleAccordion('section1');
 
 function toggleAccordion(sectionId) {
-    var content = document.getElementById(sectionId);
-    var clickedButton = document.querySelector(`.sidenav a[href="javascript:void(0)"][onclick="toggleAccordion(\'${sectionId}\')"]`);
+  // Hide all content sections
+  const allSections = document.querySelectorAll('.accordion-content');
+  allSections.forEach(section => {
+    section.style.display = 'none';
+  });
 
-    // Check if the clicked button is already active
-    var isButtonActive = clickedButton.classList.contains('active');
+  // Remove 'active' class from all buttons
+  const allButtons = document.querySelectorAll('.accordion-btn');
+  allButtons.forEach(button => {
+    button.classList.remove('active');
+  });
 
-    // Deactivate the initially active button
-    if (activeButton && !isButtonActive) {
-        activeButton.classList.remove('active');
-        hideAllAccordionContents();
-    }
+  // Show the selected content section
+  const selectedSection = document.getElementById(sectionId);
+  selectedSection.style.display = 'block';
 
-    if (!isButtonActive) {
-        content.style.display = "block";
-
-        // Mark the clicked button as active
-        clickedButton.classList.add('active');
-        activeButton = clickedButton;
-    } else {
-        // If the button is already active, you can choose to hide the content or do nothing
-        content.style.display = "none";
-        activeButton = null; // Update activeButton since the content is hidden
-    }
-}
-
-function hideAllAccordionContents() {
-    var contents = document.getElementsByClassName("accordion-content");
-    for (var i = 0; i < contents.length; i++) {
-        contents[i].style.display = "none";
-    }
-}
-
-// Initially show Section 1 content and mark the corresponding button as active
-document.getElementById("section1").style.display = "block";
-activeButton = document.querySelector('.sidenav a[href="javascript:void(0)"][onclick="toggleAccordion(\'section1\')"]');
-if (activeButton) {
-    activeButton.classList.add('active');
+  // Add 'active' class to the clicked button
+  const clickedButton = document.querySelector(`[onclick="toggleAccordion('${sectionId}')]`);
+  clickedButton.classList.add('active');
 }
 
 function toggleNav() {
@@ -46,26 +29,26 @@ function toggleNav() {
 }
 
 
-  function scrollToSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-    }
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
 }
 
 
 $('#myTab a').on('shown.bs.tab', function (e) {
-    // Remove active class from all tabs
-    $('#myTab a').removeClass('active');
-    // Add active class to the shown tab
-    $(e.target).addClass('active');
+  // Remove active class from all tabs
+  $('#myTab a').removeClass('active');
+  // Add active class to the shown tab
+  $(e.target).addClass('active');
 });
 
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
