0fe235566d0956fcef10ba26b5777291acf70897
juniordave <juniordave13@gmail.com>
2023-11-29 18:52:42 +0530

    final script commit

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ $('#myTab a').on('shown.bs.tab', function (e) {
 });
 
 // Get the button
-let mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
